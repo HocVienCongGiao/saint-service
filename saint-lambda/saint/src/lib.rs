85c471183815178cb892cf9a5c69ef96f4d8df26
ntdat02092002 <71538949+ntdat02092002@users.noreply.github.com>
2021-07-03 10:34:19 +0700

    Update saint lambda (#5)
    * hvcg_biography_openapi_saint up to 0.3.4
    
    * update
    
    * update response status

--- saint-lambda/saint/src/lib.rs
+++ saint-lambda/saint/src/lib.rs
@@ -3,7 +3,7 @@ use lambda_http::http::header::{
     ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_ORIGIN,
     CONTENT_TYPE,
 };
-use lambda_http::http::HeaderValue;
+use lambda_http::http::{method, uri::Uri, HeaderValue};
 use lambda_http::{handler, Body, Context, IntoResponse, Request, Response};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
@@ -39,7 +39,25 @@ struct TokenPayload {
 }
 */
 
+pub fn get_id_from_uri(uri: &Uri) -> Option<uuid::Uuid> {
+    let uri_path = std::path::Path::new(uri.path());
+    if !uri_path.ends_with("saints") {
+        println!("id found");
+        let id_str = uri_path.file_name().unwrap().to_str().unwrap();
+        Some(uuid::Uuid::parse_str(id_str).unwrap())
+    } else {
+        println!("id not found");
+        None
+    }
+}
+
 pub async fn saint(req: Request, ctx: Context) -> Result<impl IntoResponse, Error> {
+    if req.method() == method::Method::OPTIONS {
+        return Ok(Response::builder()
+            .body(Body::Empty)
+            .expect("unable to build http::Response"));
+    }
+
     // `serde_json::Values` impl `IntoResponse` by default
     let default_header_value = HeaderValue::from_str("Bearer eyJraWQiOiJaTGpneG41SStaZEpldnJRb0lpMTZEWEZoRHI4eG9UbVZ2b2ZuVm5vb3RFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmZDlhN2FmOC1mYTc2LTRiODYtYWYzZC1kOTYzNGVmNTIzNzQiLCJhdWQiOiIxcmF2NDExbmNjbnA3M2h0b3BiaG1sOHM2MSIsImNvZ25pdG86Z3JvdXBzIjpbIk9wZXJhdG9yR3JvdXAiXSwiZXZlbnRfaWQiOiI5NjQ1ZDYyMi0zZjRiLTQyYjctOWI0ZC03MWQzNWRhOTI1NmQiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTYyMzkzNDkyNiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLXNvdXRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLXNvdXRoZWFzdC0xXzlRV1NZR3pYayIsInBob25lX251bWJlcl92ZXJpZmllZCI6dHJ1ZSwiY29nbml0bzp1c2VybmFtZSI6ImRldi1vcGVyYXRvciIsInBob25lX251bWJlciI6Iis4NDM2OTE0MDkxNiIsImV4cCI6MTYyMzk0ODAwOCwiaWF0IjoxNjIzOTQ0NDA4fQ.ml3N8J7uw4rbQOneEdnmQW6OwsAY6ycmp5PIrKGZKF3yWQn0oQECIhF2Q_jjWOjWPikpUQEy5IKgghiJLukgKo7q-T4tUauPG3GJxoSGQkfVcglkNu8nZTu7ioxXzlQAWsXLakgkH40mGzI6kl2hkEhRQh_lWGrT7TqDP2yVTsDMKEGJBdtcb-kFCnYHfn9FMoCyVGo4K3tSrkeGno7bzwO_XpFtZRhv9Qs4OtfESXARYCP3St69hyf4JuAop6-Zb38FPWcp6rnpRG3BF64YPGqo0J0MAyWVz_Du7Pk3-H5uZqqrr6iHKoPwoabPPlZxJ3JGdifVt_I54SwTbelbzw").unwrap();
     let auth_header_value = req
@@ -49,7 +67,7 @@ pub async fn saint(req: Request, ctx: Context) -> Result<impl IntoResponse, Erro
     let auth_header_str = auth_header_value.to_str().unwrap();
     let username: String;
     let groups: Vec<String>;
-    if (auth_header_str != "anonymous12") {
+    if auth_header_str != "anonymous12" {
         let jwt_token = &auth_header_str.to_string()[7..];
         let token_data: TokenData<TokenPayload> =
             jsonwebtoken::dangerous_insecure_decode(jwt_token).unwrap();
@@ -73,13 +91,26 @@ pub async fn saint(req: Request, ctx: Context) -> Result<impl IntoResponse, Erro
         }
     );
 
-    let id = uuid::Uuid::parse_str("40e6215db5c64896987cf30f3678f608").unwrap();
-    let saint_response = controller::get_saint(id).await.unwrap();
+    let saint_response: Option<controller::openapi::saint::Saint>;
+    match *req.method() {
+        method::Method::GET => {
+            if let Some(id) = get_id_from_uri(req.uri()) {
+                saint_response = controller::get_saint(id).await;
+            } else {
+                println!("id not found");
+                // get_saints();
+                saint_response = None;
+            }
+        }
+        _ => saint_response = None,
+    }
+
     let response: Response<Body> = Response::builder()
         .header(CONTENT_TYPE, "application/json")
         .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
         .header(ACCESS_CONTROL_ALLOW_HEADERS, "*")
         .header(ACCESS_CONTROL_ALLOW_METHODS, "*")
+        .status(if saint_response == None { 404 } else { 200 })
         .body(
             serde_json::to_string(&saint_response)
                 .expect("unable to serialize serde_json::Value")
