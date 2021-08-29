d37227bebdfab9695210a434a260b57dd26df6b5
ntdat02092002 <datnt030902@gmail.com>
2021-08-29 10:39:43 +0700

    update

--- saint-app/db-postgres/src/saint_gateway/query.rs
+++ saint-app/db-postgres/src/saint_gateway/query.rs
@@ -20,33 +20,17 @@ pub async fn get_collection(
     count: i64,
     offset: i64,
 ) -> Result<Vec<Row>, Error> {
-    let statement: String;
-    if is_male.is_some() {
-        statement = format!(
-            "SELECT * FROM saint__saint_view \
-            WHERE display_name LIKE $1 AND is_male is $2 \
-            ORDER BY id \
-            LIMIT $3 OFFSET $4",
-        );
+    let statement = format!(
+        "SELECT * FROM saint__saint_view \
+        WHERE display_name LIKE $1 AND ($2::BOOL is null or is_male is $2::BOOL) \
+        ORDER BY id \
+        LIMIT $3 OFFSET $4",
+    );
 
-        println!("statement = {}", statement);
-        let stmt = (*client).prepare(&statement).await.unwrap();
-        let name_param: &[&(dyn ToSql + Sync)] =
-            &[&display_name, &is_male.unwrap(), &count, &offset];
-        client.query(&stmt, name_param).await
-    } else {
-        statement = format!(
-            "SELECT * FROM saint__saint_view \
-            WHERE display_name LIKE $1 \
-            ORDER BY id \
-            LIMIT $2 OFFSET $3",
-        );
-
-        println!("statement = {}", statement);
-        let stmt = (*client).prepare(&statement).await.unwrap();
-        let name_param: &[&(dyn ToSql + Sync)] = &[&display_name, &count, &offset];
-        client.query(&stmt, name_param).await
-    }
+    println!("statement = {}", statement);
+    let stmt = (*client).prepare(&statement).await.unwrap();
+    let name_param: &[&(dyn ToSql + Sync)] = &[&display_name, &is_male, &count, &offset];
+    client.query(&stmt, name_param).await
 }
 
 pub async fn count_without_limit(
@@ -55,32 +39,16 @@ pub async fn count_without_limit(
     is_male: Option<bool>,
     offset: i64,
 ) -> Result<i64, Error> {
-    let statement: String;
-    if is_male.is_some() {
-        statement = format!(
-            "SELECT COUNT(*) FROM
-            (SELECT * FROM saint__saint_view \
-            WHERE display_name LIKE $1 AND is_male is $2 \
-            ORDER BY id \
-            LIMIT ALL OFFSET $3) AS saints",
-        );
+    let statement = format!(
+        "SELECT COUNT(*) FROM
+        (SELECT * FROM saint__saint_view \
+        WHERE display_name LIKE $1 AND ($2::BOOL is null or is_male is $2::BOOL) \
+        ORDER BY id \
+        LIMIT ALL OFFSET $3) AS saints",
+    );
 
-        println!("statement = {}", statement);
-        let stmt = (*client).prepare(&statement).await.unwrap();
-        let name_param: &[&(dyn ToSql + Sync)] = &[&display_name, &is_male.unwrap(), &offset];
-        Ok(client.query_one(&stmt, name_param).await?.get("count"))
-    } else {
-        statement = format!(
-            "SELECT COUNT(*) FROM
-            (SELECT * FROM saint__saint_view \
-            WHERE display_name LIKE $1 \
-            ORDER BY id \
-            LIMIT ALL OFFSET $2) AS saints",
-        );
-
-        println!("statement = {}", statement);
-        let stmt = (*client).prepare(&statement).await.unwrap();
-        let name_param: &[&(dyn ToSql + Sync)] = &[&display_name, &offset];
-        Ok(client.query_one(&stmt, name_param).await?.get("count"))
-    }
+    println!("statement = {}", statement);
+    let stmt = (*client).prepare(&statement).await.unwrap();
+    let name_param: &[&(dyn ToSql + Sync)] = &[&display_name, &is_male, &offset];
+    Ok(client.query_one(&stmt, name_param).await?.get("count"))
 }
