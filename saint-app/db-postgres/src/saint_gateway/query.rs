a1fa7522522624d25b4cbc67c217cfc3b52a743b
ntdat02092002 <71538949+ntdat02092002@users.noreply.github.com>
2021-08-29 15:00:58 +0700

    update query (#19)
    * update query
    
    * update
    
    * update

--- saint-app/db-postgres/src/saint_gateway/query.rs
+++ saint-app/db-postgres/src/saint_gateway/query.rs
@@ -15,43 +15,40 @@ pub async fn find_one_by_id(client: &Client, id: Uuid) -> Result<Row, Error> {
 
 pub async fn get_collection(
     client: &Client,
-    filter: String,
-    pagination: String,
+    display_name: String,
+    is_male: Option<bool>,
+    count: i64,
+    offset: i64,
 ) -> Result<Vec<Row>, Error> {
     let statement = format!(
         "SELECT * FROM saint__saint_view \
-        WHERE {} \
+        WHERE display_name LIKE $1 AND ($2::BOOL is null or is_male = $2::BOOL) \
         ORDER BY id \
-        {}",
-        filter, pagination
+        LIMIT $3 OFFSET $4",
     );
 
     println!("statement = {}", statement);
     let stmt = (*client).prepare(&statement).await.unwrap();
-
-    // let stmt = block_on(stmt_future).unwrap();
-    let name_param: &[&(dyn ToSql + Sync)] = &[];
+    let name_param: &[&(dyn ToSql + Sync)] = &[&display_name, &is_male, &count, &offset];
     client.query(&stmt, name_param).await
 }
 
 pub async fn count_without_limit(
     client: &Client,
-    filter: String,
-    pagination: String,
+    display_name: String,
+    is_male: Option<bool>,
+    offset: i64,
 ) -> Result<i64, Error> {
     let statement = format!(
         "SELECT COUNT(*) FROM
         (SELECT * FROM saint__saint_view \
-        WHERE {} \
+        WHERE display_name LIKE $1 AND ($2::BOOL is null or is_male = $2::BOOL) \
         ORDER BY id \
-        {}) AS saints",
-        filter, pagination
+        LIMIT ALL OFFSET $3) AS saints",
     );
 
     println!("statement = {}", statement);
     let stmt = (*client).prepare(&statement).await.unwrap();
-
-    // let stmt = block_on(stmt_future).unwrap();
-    let name_param: &[&(dyn ToSql + Sync)] = &[];
+    let name_param: &[&(dyn ToSql + Sync)] = &[&display_name, &is_male, &offset];
     Ok(client.query_one(&stmt, name_param).await?.get("count"))
 }
