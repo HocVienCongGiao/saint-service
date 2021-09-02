a00fc77d4c754962cb7501c503a5008c89c76a68
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-09-03 01:39:33 +0700

    Change Terraform config to facilitate new deployment workspaces (#21)
    * Change S3 bucket name to builds-... and file name to service-name
    
    * Update gh action
    
    * Update TF
    
    * Let's try and fix 204 Content-Type
    
    * Clean up build-push.yml Gh workflow
    
    * Delete terraform.auto.tfvars
    
    * app_version
    
    Co-authored-by: binh.le-thanh (Peter Bean) <binh.le-thanh@moxbank.com>

--- saint-lambda/terraform/variables.tf
+++ saint-lambda/terraform/variables.tf
@@ -1,4 +1,10 @@
-variable "service_name" {}
+variable "service_name" {
+default = "saint-service"
+}
+
+variable "app_version" {
+  default = "latest"
+}
 
 variable "organisation" {
   default = "HocVienCongGiao"
@@ -7,6 +13,14 @@ variable "organisation" {
 variable "environment" {
   default = "dev-sg"
 }
+
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
 
 variable "db_host" {}
 variable "db_user" {}
