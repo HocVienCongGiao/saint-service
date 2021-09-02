29ed61dea98c16a8dfe6ea59871a6de6714fedf7
binh.le-thanh (Peter Bean) <binh.le-thanh@moxbank.com>
2021-09-03 01:27:37 +0700

    So tired

--- saint-lambda/terraform/variables.tf
+++ saint-lambda/terraform/variables.tf
@@ -14,6 +14,16 @@ variable "environment" {
   default = "dev-sg"
 }
 
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "aws_account_id" {}
+
+
 variable "db_host" {}
 variable "db_user" {}
 variable "db_password" {}
